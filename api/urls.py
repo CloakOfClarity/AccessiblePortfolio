7e281da93b8de96c879f48190b60fb41fbad912c
CloakOfClarity <steve@stephenhayesonline.com>
2022-05-06 01:55:52 +1000

    Added the URLs for the api.

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+	path('education', views.EducationList.as_view()),
+	path('education/<int:pk>', views.EducationByID.as_view()),
+	path('employment', views.EmploymentList.as_view()),
+	path('employment/<int:pk>', views.EmploymentByID.as_view()),
+	path('skills', views.SkillsList.as_view()),
+	path('skills/<int:pk>', views.SkillByID.as_view()),
+]
