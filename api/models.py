76fd5a577594a97260de4bf7c4e940eb21f8cd09
CloakOfClarity <steve@stephenhayesonline.com>
2022-05-06 01:43:44 +1000

    Created and migrated the models for the api.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Education(models.Model):
+	diploma = models.CharField(
+		max_length=50
+	)
+	institution = models.CharField(
+		max_length=50
+	)
+	startYear = models.PositiveSmallIntegerField()
+	endYear = models.PositiveSmallIntegerField()
+
+class Employment(models.Model):
+	role = models.CharField(
+		max_length=50
+	)
+	workplace = models.CharField(
+		max_length=50
+	)
+	startYear = models.PositiveSmallIntegerField()
+	endYear = models.PositiveSmallIntegerField()
+	responsibilities = models.TextField()
+
+class Skills(models.Model):
+	name = models.CharField(
+		max_length=50,unique=True
+	)
+	proficiency = models.PositiveSmallIntegerField(
+		choices=[
+			(1,'Basic'),
+			(2,'Regular'),
+			(3,'Expert')
+		],
+		default=2
+	)
